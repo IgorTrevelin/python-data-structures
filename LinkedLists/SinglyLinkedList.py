ee04dca0eef52cdef14a4b3fc365f7c2bcae4202
Igor Trevelin <igor.trevelin.xavier@gmail.com>
2022-06-25 18:34:41 -0300

    fixes and tests of SinglyLinkedList

--- LinkedLists/SinglyLinkedList.py
+++ LinkedLists/SinglyLinkedList.py
@@ -1,193 +1,203 @@
 class SinglyLinkedList():
 
-  class Node:
-    """
-    Represents a SinglyLinkedList node
-    """
-    def __init__(self, data):
-      self.data = data
-      self.next = None
+    class Node:
+        """
+        Represents a SinglyLinkedList node
+        """
 
-    def set_next(self, node):
-      """
-      Changes the next node pointer value
-      """
-      self.next = node
+        def __init__(self, data):
+            self.data = data
+            self.next = None
 
-    def get_next(self):
-      """
-      Returns the next node pointer value
-      """
-      return self.next
+        def set_next(self, node):
+            """
+            Changes the next node pointer value
+            """
+            self.next = node
 
-    def get_data(self):
-      """
-      Returns the node data value
-      """
-      return self.data
+        def get_next(self):
+            """
+            Returns the next node pointer value
+            """
+            return self.next
 
-    def set_data(self, value):
-      """
-      Changes the node data value
-      """
-      self.data = value
+        def get_data(self):
+            """
+            Returns the node data value
+            """
+            return self.data
 
+        def set_data(self, value):
+            """
+            Changes the node data value
+            """
+            self.data = value
 
-  def __init__(self):
-    self.head = None
-    self.length = 0
+    def __init__(self):
+        self.head = None
+        self.length = 0
 
-  def push(self, value):
-    """
-    Inserts new node at the beginning of the list
-    """
-    new_node = self.Node(value)
-    new_node.set_next(self.head)
+    def push(self, value):
+        """
+        Inserts new node at the beginning of the list
+        """
+        new_node = self.Node(value)
+        new_node.set_next(self.head)
 
-    self.head = new_node
-    
-    self.length += 1
+        self.head = new_node
 
-  def append(self, value):
-    """
-    Inserts new node at the end of the list
-    """
-    new_node = self.Node(value)
+        self.length += 1
 
-    current_node = self.head
-    if current_node is None:
-      self.head = new_node
-    else:
-      while current_node.get_next() is not None:
-        current_node = current_node.get_next()
+    def append(self, value):
+        """
+        Inserts new node at the end of the list
+        """
+        new_node = self.Node(value)
 
-      current_node.set_next(new_node)
-
-    self.length += 1
-
-  def remove_by_index(self, index):
-    """
-    Removes element by index
-    """
-    if index >= self.length or index < 0:
-      raise IndexError('List index out of bounds')
-
-    i = 0
-    previous_node = None
-    current_node = self.head
-
-    while i != index:
-      previous_node = current_node
-      current_node = current_node.get_next()
-      i += 1
-
-    if current_node == self.head:
-      self.head = current_node.get_next()
-    else:
-      previous_node.set_next(current_node.get_next())
-
-    self.length -= 1
-
-  def remove_by_value(self, value, single_value = True):
-    """
-    Removes element by value
-    """
-    current_node = self.head
-    previous_node = None
-
-    while current_node is not None:
-      if current_node.get_data() == value:
-        if current_node == self.head:
-          self.head = current_node.get_next()
+        current_node = self.head
+        if current_node is None:
+            self.head = new_node
         else:
-          previous_node.set_next(current_node.get_next())
+            while current_node.get_next() is not None:
+                current_node = current_node.get_next()
+
+            current_node.set_next(new_node)
+
+        self.length += 1
+
+    def remove_by_index(self, index):
+        """
+        Removes element by index
+        """
+        if index >= self.length or index < 0:
+            raise IndexError('List index out of bounds')
+
+        i = 0
+        previous_node = None
+        current_node = self.head
+
+        while i != index:
+            previous_node = current_node
+            current_node = current_node.get_next()
+            i += 1
+
+        if current_node == self.head:
+            self.head = current_node.get_next()
+            del current_node
+            current_node = self.head
+        else:
+            previous_node.set_next(current_node.get_next())
+            del current_node
+            current_node = previous_node.get_next()
+
         self.length -= 1
 
-      if single_value:
-        break
+    def remove_by_value(self, value, single_value=True):
+        """
+        Removes element by value
+        """
+        current_node = self.head
+        previous_node = None
 
-      previous_node = current_node
-      current_node = current_node.get_next()
+        while current_node is not None:
+            if current_node.get_data() == value:
+                if current_node == self.head:
+                    self.head = current_node.get_next()
+                    del current_node
+                    current_node = self.head
+                else:
+                    previous_node.set_next(current_node.get_next())
+                    del current_node
+                    current_node = previous_node.get_next()
 
-  def get_by_index(self, index):
-    """
-    Returns the data value of the node at the specified index
-    """
-    if index < 0 or index >= self.length:
-      raise IndexError('List index out of bounds')
+                self.length -= 1
 
-    i = 0
-    current_node = self.head
+                if single_value:
+                    break
 
-    while i < index:
-      current_node = current_node.get_next()
+            else:
+                previous_node = current_node
+                current_node = current_node.get_next()
 
-    return current_node.get_data()
+    def get_by_index(self, index):
+        """
+        Returns the data value of the node at the specified index
+        """
+        if index < 0 or index >= self.length:
+            raise IndexError('List index out of bounds')
 
-  def index_of(self, value):
-    """
-    Returns the node index of the first occurrence of the value in the list
-    """
+        i = 0
+        current_node = self.head
 
-    i = 0
-    current_node = self.head
+        while i < index:
+            current_node = current_node.get_next()
+            i += 1
 
-    if current_node.get_data() == value:
-      return i
+        return current_node.get_data()
 
-    while current_node.get_next() is not None:
-      current_node = current_node.get_next()
-      i += 1
+    def index_of(self, value):
+        """
+        Returns the node index of the first occurrence of the value in the list. If the value doesn't exist in the list, returns -1
+        """
 
-      if current_node.get_data() == value:
-        return i
+        i = 0
+        current_node = self.head
 
-    return -1
+        if current_node.get_data() == value:
+            return i
 
-  def update_by_index(self, index, value):
-    """
-    Updates the data value of the node at the specified index of the list
-    """
+        while current_node.get_next() is not None:
+            current_node = current_node.get_next()
+            i += 1
 
-    if index < 0 or index >= self.length:
-      raise IndexError('List index out of bounds')
+            if current_node.get_data() == value:
+                return i
 
-    i = 0
-    current_node = self.head
+        return -1
 
-    if i == index:
-      current_node.set_data(value)
+    def update_by_index(self, index, value):
+        """
+        Updates the data value of the node at the specified index of the list
+        """
 
-    while current_node.get_next() is not None:
-      current_node = current_node.get_next()
-      i += 1
+        if index < 0 or index >= self.length:
+            raise IndexError('List index out of bounds')
 
-      if i == index:
-        current_node.set_data(value)
-        return
+        i = 0
+        current_node = self.head
 
+        if i == index:
+            current_node.set_data(value)
 
-  def __len__(self):
-    """
-    Returns the current length of the list when the list is passed to len function
-    """
-    return self.length
+        while current_node.get_next() is not None:
+            current_node = current_node.get_next()
+            i += 1
 
-  def __str__(self):
-    """
-    Returns textual notation of the list
-    """
-    current_node = self.head
-    _str = '['
-    
-    while current_node is not None:
-      _str += str(current_node.get_data())
+            if i == index:
+                current_node.set_data(value)
+                return
 
-      if current_node.get_next() is not None:
-        _str += ', '
+    def __len__(self):
+        """
+        Returns the current length of the list when the list is passed to len function
+        """
+        return self.length
 
-      current_node = current_node.get_next()
+    def __str__(self):
+        """
+        Returns textual notation of the list
+        """
+        current_node = self.head
+        _str = '['
 
-    _str += ']'
+        while current_node is not None:
+            _str += str(current_node.get_data())
 
-    return _str
+            if current_node.get_next() is not None:
+                _str += ', '
+
+            current_node = current_node.get_next()
+
+        _str += ']'
+
+        return _str
---
+++ tests/test_SinglyLinkedList.py
@@ -0,0 +1,119 @@
+import unittest
+
+from LinkedLists.SinglyLinkedList import SinglyLinkedList
+
+def list_push_many(list, *args):
+    for arg in args:
+        list.push(arg)
+
+def list_append_many(list, *args):
+    for arg in args:
+        list.append(arg)
+
+class TestSinglyLinkedList(unittest.TestCase):
+    def test_push(self):
+        list = SinglyLinkedList()
+        list_push_many(list, 5, 4, 3, 2, 1)
+        self.assertEqual(str(list), '[1, 2, 3, 4, 5]')
+
+    def test_append(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 1, 2, 3, 4, 5)
+        self.assertEqual(str(list), '[1, 2, 3, 4, 5]')
+
+    def test_remove_by_index(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5)
+
+        list.remove_by_value(1)
+        self.assertEqual(str(list), '[1, 1, 2, 3, 3, 3, 4, 5, 5, 5]')
+
+        list.remove_by_value(1, False)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4, 5, 5, 5]')
+
+        list.remove_by_value(5)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4, 5, 5]')
+
+        list.remove_by_value(5, False)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4]')
+
+        list.remove_by_value(3)
+        self.assertEqual(str(list), '[2, 3, 3, 4]')
+
+        list.remove_by_value(3, False)
+        self.assertEqual(str(list), '[2, 4]')
+
+    def test_remove_by_value(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5)
+
+        list.remove_by_value(1)
+        self.assertEqual(str(list), '[1, 1, 2, 3, 3, 3, 4, 5, 5, 5]')
+
+        list.remove_by_value(1, False)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4, 5, 5, 5]')
+
+        list.remove_by_value(5)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4, 5, 5]')
+
+        list.remove_by_value(5, False)
+        self.assertEqual(str(list), '[2, 3, 3, 3, 4]')
+
+        list.remove_by_value(3)
+        self.assertEqual(str(list), '[2, 3, 3, 4]')
+
+        list.remove_by_value(3, False)
+        self.assertEqual(str(list), '[2, 4]')
+
+    def test_get_by_index(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 11, 26, 30, 49, 59, 36, 31, 44, 57, 51, 55)
+
+        self.assertEqual(list.get_by_index(0), 11)
+        self.assertEqual(list.get_by_index(1), 26)
+        self.assertEqual(list.get_by_index(10), 55)
+
+        with self.assertRaises(IndexError):
+            list.get_by_index(-1)
+
+        with self.assertRaises(IndexError):
+            list.get_by_index(11)
+
+    def test_index_of(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 11, 26, 30, 49, 59, 36, 31, 44, 57, 51, 55)
+
+        self.assertEqual(list.index_of(11), 0)
+        self.assertEqual(list.index_of(55), 10)
+        self.assertEqual(list.index_of(26), 1)
+
+    def test_update_by_index(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 11, 26, 30, 49, 59, 36, 31, 44, 57, 51, 55)
+
+        list.update_by_index(0, 2)
+        self.assertEqual(str(list), '[2, 26, 30, 49, 59, 36, 31, 44, 57, 51, 55]')
+
+        list.update_by_index(1, 29)
+        self.assertEqual(str(list), '[2, 29, 30, 49, 59, 36, 31, 44, 57, 51, 55]')
+
+        list.update_by_index(10, 11)
+        self.assertEqual(str(list), '[2, 29, 30, 49, 59, 36, 31, 44, 57, 51, 11]')
+
+        list.update_by_index(9, 100)
+        self.assertEqual(str(list), '[2, 29, 30, 49, 59, 36, 31, 44, 57, 100, 11]')
+
+    def test_len(self):
+        list = SinglyLinkedList()
+        list_append_many(list, 11, 26, 30, 49, 59, 36, 31, 44, 57, 51, 55)
+
+        self.assertEqual(len(list), 11)
+
+        list.remove_by_index(0)
+        self.assertEqual(len(list), 10)
+
+        list.remove_by_index(0)
+        self.assertEqual(len(list), 9)
+
+        list.push(111)
+        self.assertEqual(len(list), 10)
